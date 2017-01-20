58df3701c941917c0210d67e38c60cc251e3e986
Noah Fernandes <noahdecoco@gmail.com>
2017-01-21 00:06:44 +0100

    Created a basic player movement

--- GGJ2017/Assets/Scripts/PlayerMovement.cs
+++ GGJ2017/Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public float moveSpeed = 10f;
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        
+		if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        
+		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+			transform.Translate(Vector3.right * -moveSpeed * Time.deltaTime);
+        
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
 	}
 }
