c444eae07072858b8704afc8442f5fa3c98a30b0
Casper <cmvanb@gmail.com>
2017-01-21 13:41:23 +0100

    player movement w keyboard

--- GGJ2017/Assets/Scripts/PlayerMovement.cs
+++ GGJ2017/Assets/Scripts/PlayerMovement.cs
@@ -2,89 +2,111 @@
 using System.Collections;
 using XInputDotNetPure; // Required in C#
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    // Public vars.
+    public float moveSpeed = 10f;
 
-    bool playerIndexSet = false;
-    PlayerIndex playerIndex;
-    GamePadState state;
-    GamePadState prevState;
-    
-	public float moveSpeed = 10f;
+    public bool useKeyboardControls = false;
+
+    // Static (global) vars. Don't use globals, except for the GLOBAL GAME JAM!!!
+    public static bool[] ClaimedPlayerIndices = new bool[] { false, false, false, false };
+
+    // Input vars.
+    private bool playerIndexSet = false;
+
+    private PlayerIndex playerIndex;
+
+    private GamePadState state;
+
+    private GamePadState prevState;
+
+    // Movement vars.
 	private bool _isGrounded = true;
+
 	private Rigidbody _rigidBody;
 
-	void Start() {
-
+	void Start()
+    {
 		_rigidBody = GetComponent<Rigidbody>();
-
 	}
 
-	void OnTriggerEnter(Collider other) {
-		
-		if(other.tag == "MovingPlatform"){
+	void OnTriggerEnter(Collider other)
+    {
+		if (other.tag == "MovingPlatform")
+        {
 			_isGrounded = true;
 		}
-
 	}
 
-	void OnTriggerExit(Collider other) {
-		
-		if(other.tag == "MovingPlatform"){
+	void OnTriggerExit(Collider other)
+    {
+		if (other.tag == "MovingPlatform")
+        {
 			_isGrounded = false;
 		}
-
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		HandleInput();
-
-		//if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-		//	transform.Translate(Vector3.forward.normalized * moveSpeed * Time.deltaTime, Space.World);
-        
-		//if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-		//	transform.Translate(-Vector3.forward.normalized * moveSpeed * Time.deltaTime, Space.World);
-        
-		//if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-		//	transform.Translate(Vector3.right.normalized * -moveSpeed * Time.deltaTime, Space.World);
-        
-		//if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-		//	transform.Translate(Vector3.right.normalized * moveSpeed * Time.deltaTime, Space.World);
-
-		//if(Input.GetKey(KeyCode.Space) && _isGrounded) {
-		//	print("Jump");
-		//	// transform.Translate(Vector3.up * 12 * Time.deltaTime, Space.World);
-		//	// _rigidBody.AddForce(Vector3.up * 5000);
-		//}
 	}
 
 	void HandleInput()
 	{
-        // Find a PlayerIndex, for a single player game
-        // Will find the first controller that is connected ans use it
-        if (!playerIndexSet || !prevState.IsConnected)
+        Vector3 direction;
+
+        if (useKeyboardControls)
         {
-            for (int i = 0; i < 4; ++i)
+            direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        }
+        // Gamepad controls.
+        else
+        {
+            if (playerIndexSet && !prevState.IsConnected)
             {
-                PlayerIndex testPlayerIndex = (PlayerIndex)i;
-                GamePadState testState = GamePad.GetState(testPlayerIndex);
-                if (testState.IsConnected)
+                ClaimedPlayerIndices[(int)playerIndex] = false;
+
+                playerIndexSet = false;
+            }
+
+            if (!playerIndexSet)
+            {
+                for (int i = 0; i < 4; ++i)
                 {
-                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
-                    playerIndex = testPlayerIndex;
-                    playerIndexSet = true;
+                    PlayerIndex testPlayerIndex = (PlayerIndex)i;
+
+                    GamePadState testState = GamePad.GetState(testPlayerIndex);
+
+                    // Make sure controller is connected and not claimed yet.
+                    if (testState.IsConnected && ClaimedPlayerIndices[i] == false)
+                    {
+                        Debug.Log(ClaimedPlayerIndices[i]);
+                        Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+
+                        playerIndex = testPlayerIndex;
+
+                        playerIndexSet = true;
+
+                        ClaimedPlayerIndices[i] = true;
+                        Debug.Log(ClaimedPlayerIndices[i]);
+                    }
                 }
+            }
+
+            if (playerIndexSet)
+            {
+                prevState = state;
+
+                state = GamePad.GetState(playerIndex);
+
+                direction = new Vector3(state.ThumbSticks.Left.X, 0f, state.ThumbSticks.Left.Y);
             }
         }
 
-        prevState = state;
-        state = GamePad.GetState(playerIndex);
-
-        var direction = new Vector3(state.ThumbSticks.Left.X, 0f, state.ThumbSticks.Left.Y);
-
         transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
-
-        // Make the current object turn
-        //transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
     }
+
+    
 }	
