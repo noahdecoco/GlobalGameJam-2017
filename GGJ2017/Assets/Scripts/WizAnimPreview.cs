7615accb35ffede1ea8aee584b9ab53c7f4f9907
deboon <daan@daanboon.nl>
2017-01-21 21:40:56 +0100

    anim preview test

---
+++ GGJ2017/Assets/Scripts/WizAnimPreview.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizAnimPreview : MonoBehaviour
+{
+    public enum Anims
+    {
+        idle,
+        walk,
+        magic
+    }
+
+    public Anims setAnim = Anims.idle;
+    Animator myAnim = null;
+
+	void Start ()
+    {
+        myAnim = GetComponent<Animator>();
+	}
+	
+	void LateUpdate ()
+    {
+		switch(setAnim)
+        {
+            case Anims.idle:
+                myAnim.SetTrigger("idle");
+                break;
+            case Anims.magic:
+                myAnim.SetTrigger("magic");
+                break;
+            case Anims.walk:
+                myAnim.SetTrigger("walk");
+                break;
+        }
+	}
+}
