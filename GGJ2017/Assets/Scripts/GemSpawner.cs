ef15675e8fe3ab1b8fc6a02542726e7d5a4d7f89
Casper <cmvanb@gmail.com>
2017-01-21 23:49:37 +0100

    working on gem spawning

---
+++ GGJ2017/Assets/Scripts/GemSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    // Public vars.
+    public int MaxGemCount = 100;
+
+    public float AverageTimeBetweenGemWaves = 10f;
+
+    public float VariationInTimeBetweenGemWaves = 1f;
+
+    public GameObject GemPrefab;
+
+    // Private vars.
+    private List<GameObject> activeGems = new List<GameObject>();
+
+    private PlatformManager platformManager;
+
+    // Unity callbacks.
+	void Start()
+    {
+        platformManager = GetComponent<PlatformManager>();
+
+        SpawnGemWave();
+    }
+
+    // Private methods.
+    private void SpawnGemWave()
+    {
+        int newGemCount = MaxGemCount - activeGems.Count;
+
+        for (int i = 0; i < newGemCount; ++i)
+        {
+            SpawnGem();
+        }
+
+        float timeUntilNextGemWave = AverageTimeBetweenGemWaves + Random.Range(-VariationInTimeBetweenGemWaves, VariationInTimeBetweenGemWaves);
+
+        Invoke("SpawnGemWave", timeUntilNextGemWave);
+    }
+
+    private void SpawnGem()
+    {
+        // pick platform
+        int index = (int)(Random.value * platformManager.Platforms.Count);
+
+        GameObject platform = platformManager.Platforms[index];
+
+        // pick position offset
+        float angle = 0f;
+
+        float distance = 10f;
+
+        // instantiate
+        GameObject newGem = GameObject.Instantiate(GemPrefab);
+
+        newGem.transform.position = new Vector3(0f, 0f, 0f);
+    }
+}
--- GGJ2017/Assets/Scripts/PlatformManager.cs
+++ GGJ2017/Assets/Scripts/PlatformManager.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformManager : MonoBehaviour {
+public class PlatformManager : MonoBehaviour
+{
+    // Public vars.
+    public List<GameObject> Platforms = new List<GameObject>();
 
-	// Use this for initialization
-	void Start () {
-		
+	// Unity callbacks.
+	void Start ()
+    {
+        // Populate list.
+        GameManager gameManager = GetComponent<GameManager>();
+
+        GameObject levelObject = gameManager.LevelObject;
+
+        Platforms.AddRange(FindAllPlatformsRecursively(levelObject.transform));
+
+        Debug.Log("Found platforms: " + Platforms.Count);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Private methods.
+    private List<GameObject> FindAllPlatformsRecursively(Transform t)
+    {
+        List<GameObject> platformsFound = new List<GameObject>();
+
+        foreach (Transform child in t)
+        {
+            if (child.tag == "MovingPlatform")
+            {
+                platformsFound.Add(child.gameObject);
+            }
+
+            if (child.childCount > 0)
+            {
+                platformsFound.AddRange(FindAllPlatformsRecursively(child));
+            }
+        }
+
+        return platformsFound;
+    }
 }
