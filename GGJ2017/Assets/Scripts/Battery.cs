27ae3eda0fea55306aa59640bf5ba55ef81db958
Casper <cmvanb@gmail.com>
2017-01-21 16:59:17 +0100

    working on crystal charge mechanic

---
+++ GGJ2017/Assets/Scripts/Battery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    // Events.
+    public delegate void ChargeValueChangedHandler(object sender, float chargeValue);
+    public event ChargeValueChangedHandler ChargeValueChangedEvent;
+
+    // Public vars.
+    public float ChargeValue { get { return chargeValue; } }
+
+    // Private vars.
+    private float chargeValue = 1f;
+
+    public void Charge(float percentage)
+    {
+        chargeValue += percentage;
+
+        // Don't go over 100%.
+        chargeValue = Mathf.Min(chargeValue, 1f);
+
+        ChargeValueChangedEvent(this, chargeValue);
+    }
+
+    public void Drain(float percentage)
+    {
+        chargeValue -= percentage;
+
+        // Don't go under 0%.
+        chargeValue = Mathf.Max(chargeValue, 0f);
+
+        ChargeValueChangedEvent(this, chargeValue);
+    }
+}
---
+++ GGJ2017/Assets/Scripts/CrystalView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalView : MonoBehaviour
+{
+    // Private vars.
+    private Battery battery;
+
+    private Material crystalMaterial;
+
+	void Start ()
+    {
+        battery = GetComponent<Battery>();
+        
+        battery.ChargeValueChangedEvent += OnChargeValueChanged;
+
+        crystalMaterial = transform.FindChild("chrystal").GetComponent<Material>();
+    }
+
+    void OnChargeValueChanged(object sender, float chargeValue)
+    {
+        // TODO: Insert more code affecting how crystal looks here.
+
+        // Temporary effect until artists change this - sets material alpha equal to charge value.
+        crystalMaterial.color = new Color(
+            crystalMaterial.color.r, 
+            crystalMaterial.color.g, 
+            crystalMaterial.color.b, 
+            chargeValue);
+    }
+}
