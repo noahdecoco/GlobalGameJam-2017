768497636923bf08d734ddf9c393454d58e605da
Noah Fernandes <noahdecoco@gmail.com>
2017-01-21 17:56:28 +0100

    Added basic respawn

--- GGJ2017/Assets/PlayerController.cs
+++ GGJ2017/Assets/PlayerController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public GameObject respawnPoint;
+	private float respawnTime = 5.0f;
+	private bool _isDead = false;
+
 
 	private void OnCollisionEnter(Collision other) {
 		if(other.gameObject.tag == "Death") {
 			print("Player dead, respawn");
+			_isDead = true;
+		}
+ 	}
+
+ 	private void Update() {
+ 		if(_isDead){
+			respawnTime -= Time.deltaTime;
+ 		}
+		if(respawnTime < 0){
+			_isDead = false;
+			respawnTime = 5.0f;
 			gameObject.transform.position = respawnPoint.transform.position;
 		}
  	}
