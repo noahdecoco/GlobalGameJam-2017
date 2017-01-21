264b5720231132f6f9f1cc353d9111863f15cace
Noah Fernandes <noahdecoco@gmail.com>
2017-01-21 17:40:19 +0100

    Added basic respawn point

---
+++ GGJ2017/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject respawnPoint;
+
+	private void OnCollisionEnter(Collision other) {
+		if(other.gameObject.tag == "Death") {
+			print("Player dead, respawn");
+			gameObject.transform.position = respawnPoint.transform.position;
+		}
+ 	}
+
+ }
