38f795a40cb6f708fee17f7c3f8ca5e578737c4e
Noah Fernandes <noahdecoco@gmail.com>
2017-01-21 17:04:57 +0100

    Level rings rotate and dissapear (needs visual cue for dissapear)

--- GGJ2017/Assets/Scripts/Platform.cs
+++ GGJ2017/Assets/Scripts/Platform.cs
@@ -5,17 +5,68 @@ using UnityEngine;
 public class Platform : MonoBehaviour {
 
 	private float _timeToDrop;
+	private float _timeToRise;
+	private bool _isDropped = false;
+	public float _minSecs = 2.0f;
+	public float _maxSecs = 20.0f;
 
-	// Use this for initialization
 	void Start () {
-		_timeToDrop = Random.Range(10.0f, 20.0f);
+
+		_timeToDrop = getRandomSecs(5.0f, 30.0f);
+		_timeToRise = getRandomSecs(3.0f, 6.0f);
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		_timeToDrop -= Time.deltaTime;
-		if(_timeToDrop < 0){
-			gameObject.SetActive(false);
+
+		CalcDrop();
+
+	}
+
+	float getRandomSecs(float min, float max) {
+
+		return Random.Range(min, max);
+
+	}
+
+	void removePlatform() {
+
+		GetComponent<MeshRenderer>().enabled = false;
+		GetComponent<MeshCollider>().enabled = false;
+
+	}
+
+	void addPlatform() {
+
+		GetComponent<MeshRenderer>().enabled = true;
+		GetComponent<MeshCollider>().enabled = true;
+
+	}
+
+	void CalcDrop () {
+
+		if(!_isDropped){
+			_timeToDrop -= Time.deltaTime;
+		} else {
+			_timeToRise -= Time.deltaTime;
 		}
+
+		if(_timeToDrop < 0){
+			if(!_isDropped){
+				removePlatform();
+				_timeToDrop = getRandomSecs(5.0f, 30.0f);
+			}
+			_isDropped = true;
+		}
+
+		if(_timeToRise < 0){
+			if(_isDropped){
+				addPlatform();
+				_timeToRise = getRandomSecs(3.0f, 6.0f);
+			}
+			_isDropped = false;
+		}
+
+
 	}
 }
--- GGJ2017/Assets/Scripts/PlayerGrounding.cs
+++ GGJ2017/Assets/Scripts/PlayerGrounding.cs
@@ -6,14 +6,14 @@ public class PlayerGrounding : MonoBehaviour {
 	private void OnCollisionEnter(Collision other) {
 
 		if(other.gameObject.tag == "MovingPlatform") {
-			print("Enter");
+//			print("Enter");
 //			transform.parent = other.transform;
 		}
  	}
 
 	private void OnCollisionExit(Collision other) {
 		if(other.gameObject.tag == "MovingPlatform") {
-			print("Leave");
+//			print("Leave");
 			if(transform.parent == other.transform){
 //				transform.parent = null;
 			}
