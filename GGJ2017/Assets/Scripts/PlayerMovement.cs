6780dc3a1a0f70c8afc37c42f01e347bdc12e6e1
Casper <cmvanb@gmail.com>
2017-01-21 12:52:21 +0100

    working on controller input

--- GGJ2017/Assets/Scripts/PlayerMovement.cs
+++ GGJ2017/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    
 	public float moveSpeed = 10f;
 	private bool _isGrounded = true;
 	private Rigidbody _rigidBody;
@@ -32,23 +37,43 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward.normalized * moveSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-			transform.Translate(-Vector3.forward.normalized * moveSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-			transform.Translate(Vector3.right.normalized * -moveSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-			transform.Translate(Vector3.right.normalized * moveSpeed * Time.deltaTime);
-
-		if(Input.GetKey(KeyCode.Space) && _isGrounded) {
-			print("Jump");
-			// transform.Translate(Vector3.up * 12 * Time.deltaTime, Space.World);
-			// _rigidBody.AddForce(Vector3.up * 5000);
-		}
-
+		HandleInput();
 	}
+
+	void HandleInput()
+	{
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        // Detect if a button was pressed this frame
+        if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
+        {
+            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+        }
+        // Detect if a button was released this frame
+        if (prevState.Buttons.A == ButtonState.Pressed && state.Buttons.A == ButtonState.Released)
+        {
+            GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+
+        // Make the current object turn
+        transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
+    }
 }	
