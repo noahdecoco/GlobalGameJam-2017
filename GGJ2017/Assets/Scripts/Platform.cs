7e126dbee1c08c1541df184cb7572f4802f76881
Noah Fernandes <noahdecoco@gmail.com>
2017-01-21 23:24:56 +0100

    Added animations to the platforms

--- GGJ2017/Assets/Scripts/Platform.cs
+++ GGJ2017/Assets/Scripts/Platform.cs
@@ -4,75 +4,67 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
+	private float _timeToShake;
 	private float _timeToDrop;
 	private float _timeToRise;
+
+	private bool _isSteady = true;
+	private bool _isShaking = false;
 	private bool _isDropped = false;
-	public float minSecs = 2.0f;
-	public float maxSecs = 20.0f;
-
-	private Vector3 _oldPosition;
-	private Vector3 _newPosition;
-
-	float lerpTime = 3.0f;
-    float currentLerpTime = 0.0f;
 
 	void Start () {
 
-		_timeToDrop = getRandomSecs(15.0f, 60.0f);
-		_timeToRise = getRandomSecs(3.0f, 6.0f);
-		_oldPosition = new Vector3(0,Random.Range(-40.0f, -80.0f),0);
-		_newPosition = new Vector3(0,0,0);
+		iTween.MoveFrom(gameObject,iTween.Hash("y",-10,"time",3,"delay",Random.Range(1.0f,3.0f)));
+		resetTimers();
+
 	}
 
 	void Update () {
-		CalcDrop();
-		updatePosition();
+
+		updateState();
 
 	}
 
-	void updatePosition() {
+	void resetTimers(){
 
-		currentLerpTime += Time.deltaTime;
-		if (currentLerpTime > lerpTime) currentLerpTime = lerpTime;
-		float perc = currentLerpTime / lerpTime;
-		//print("drop" + perc);
-		transform.position = Vector3.Lerp(_oldPosition, _newPosition, perc);
-	}
-
-	float getRandomSecs(float min, float max) {
-
-		return Random.Range(min, max);
+		_timeToShake = Random.Range(10.0f, 40.0f);
+		_timeToDrop = 2.0f;
+		_timeToRise = Random.Range(3.0f, 6.0f);
 
 	}
 
-	void CalcDrop () {
+	void updateState(){
 
-		if(!_isDropped){
+		if(_isSteady) {
+			_timeToShake -= Time.deltaTime;
+			if(_timeToShake < 0) {
+				_isSteady = false;
+				_isShaking = true;	
+			}
+			return;
+		}
+
+		if(_isShaking) {
+			transform.position = new Vector3(0, Random.Range(-0.2f, 0.0f), 0);
 			_timeToDrop -= Time.deltaTime;
-		} else {
+			if(_timeToDrop < 0) {
+				_isShaking = false;
+				_isDropped = true;
+				iTween.MoveTo(gameObject, new Vector3(0,-10,0), 2);
+			}
+			return;
+		}
+
+		if(_isDropped) {
 			_timeToRise -= Time.deltaTime;
+			if(_timeToRise < 0) {
+				_isDropped = false;
+				_isSteady = true;
+				resetTimers();
+				iTween.MoveTo(gameObject, new Vector3(0,0,0), 2);
+			}
+			return;
 		}
 
-		if(_timeToDrop < 0){
-			if(!_isDropped){
-				currentLerpTime = 0.0f;
-				lerpTime = 5.0f;
-				_oldPosition = new Vector3(0,0,0);
-				_newPosition = new Vector3(0,-12,0);
-				_timeToDrop = getRandomSecs(5.0f, 30.0f);
-			}
-			_isDropped = true;
-		}
-
-		if(_timeToRise < 0){
-			if(_isDropped){
-				currentLerpTime = 0.0f;
-				lerpTime = 3.0f;
-				_oldPosition = new Vector3(0,-12,0);
-				_newPosition = new Vector3(0,0,0);
-				_timeToRise = getRandomSecs(3.0f, 6.0f);
-			}
-			_isDropped = false;
-		}
 	}
 }
