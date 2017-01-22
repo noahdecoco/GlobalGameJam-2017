3df37736ed24a4f5cefcb275e630b848ee56a94f
Casper <cmvanb@gmail.com>
2017-01-22 14:27:48 +0100

    added rumbler and tweaked distance for interaction

---
+++ GGJ2017/Assets/Scripts/Rumbler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure; // Required in C#
+
+public class Rumbler : MonoBehaviour
+{
+    public void SetRumble(PlayerIndex playerIndex, float lowRumble, float highRumble)
+    {
+        GamePad.SetVibration(playerIndex, lowRumble, highRumble);
+    }
+
+    public void StopRumble(PlayerIndex playerIndex)
+    {
+        GamePad.SetVibration(playerIndex, 0f, 0f);
+    }
+}
